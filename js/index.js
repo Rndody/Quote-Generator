4885a00eb12754ff3bf424f6f4bb2cf712a4cce6
Rndody <rndody@gmail.com>
2023-07-15 17:37:11 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -169,38 +169,6 @@ function generateQuote() {
 
 
 
-// var numbers = []; // this array will store the available numbers..
-
-//         function generateNumbers()
-//         {
-//             // populate the available numbers however you need to..
-//             for(var i=0; i<360; i+=30)
-//             {
-//                 numbers.push(i);
-//             }
-//         }
-
-
-
-
-// function spin()
-//         {
-//             if(numbers.length==0)
-//             {
-//                 // then we've used  up all available numbers..start new game or whatever you need to do..
-//                 alert("starting again");
-//                 generateNumbers();
-//             }
-//             var rand = Math.floor(Math.random()*numbers.length);
-//              // select an index randomly based on the number of remaining available numbers..
-//             var num = numbers[rand];
-//             numbers.splice(rand,1);
-//              // remove the number we selected so it can't be selected next time..
-//             document.getElementById("number").innerHTML = num;
-//         }
-
-
-
 
 
 
